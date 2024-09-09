51e6d4cd9f839b503622b9ba9c74c8273fb6635c
Cyb3r-Bishop <ariffoysalruetcse21@gmail.com>
2024-09-09 19:28:34 +0600

    Update
    code 1st
    
    Co-Authored-By: Triple-mm <180957717+Triple-mm@users.noreply.github.com>

---
+++ h.cpp
@@ -0,0 +1,36 @@
+//
+//
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vll vector <long long int>
+#define vin vector <int>
+#define pb() push_back()
+#define ff(i, n) for(int i = 0; i < n; i++)
+#define fb(i, n) for(int i = n; i > 0; i--)
+#define py cout << "YES" << endl;
+#define pn cout << "NO" << endl;
+#define Code_By_Arif_Foysal  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const int M = 1e9+7;
+const int N = 1e5+10;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    Code_By_Arif_Foysal
+
+    int t; cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
